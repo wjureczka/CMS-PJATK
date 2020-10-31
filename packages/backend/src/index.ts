4658a87c948a3de16aea3173a34cf5c9c1737007
wjureczka <wjureczka@gmail.com>
2020-10-31 22:27:20 +0100

    Build: Proper alias resolving with tsconfig-paths

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -8,5 +8,5 @@ const app = express();
 app.get('/', (req, res) => res.send('Express + TypeScript Server'));
 
 app.listen(SERVER_PORT, () => {
-    console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
+    console.log(`⚡️[server]: Server is running at https://localhost:${SERVER_PORT}`);
 });
