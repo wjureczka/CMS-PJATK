76df0e756b9dd1b632331e9563f5ea21d47c8324
wjureczka <wjureczka@gmail.com>
2020-11-01 00:01:25 +0100

    Done

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -5,7 +5,7 @@ import { SERVER_PORT } from 'src/config';
 const app = express();
 
 
-app.get('/', (req, res) => res.send('Express + TypeScript Server: CMS PJATK PL'));
+app.get('/', (req, res) => res.send('Express + TypeScript Server: CMS PJATK'));
 
 app.listen(SERVER_PORT, () => {
     console.log(`⚡️[server]: Server is running at http://localhost:${SERVER_PORT}`);
