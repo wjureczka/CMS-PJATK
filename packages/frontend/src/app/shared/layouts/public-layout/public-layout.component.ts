54932cf4d9074d761a4a3e4f44af7ad90167294a
bdom5ki <s16014@pjwstk.edu.pl>
2020-11-23 17:49:22 +0100

    Create lazy modules

--- packages/frontend/src/app/shared/layouts/public-layout/public-layout.component.ts
+++
@@ -1,9 +0,0 @@
-import {Component} from '@angular/core';
-
-@Component({
-  selector: 'app-auth-layout',
-  templateUrl: './public-layout.component.html',
-  styleUrls: ['./public-layout.component.scss']
-})
-export class PublicLayoutComponent {
-}
