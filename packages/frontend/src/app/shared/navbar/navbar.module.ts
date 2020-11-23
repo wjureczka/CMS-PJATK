bbd09e8eb944c399bc25a095f502c5bc6e72f22a
bdom5ki <s16014@pjwstk.edu.pl>
2020-11-23 17:53:34 +0100

    Attach routing to navbar

--- packages/frontend/src/app/shared/navbar/navbar.module.ts
+++ packages/frontend/src/app/shared/navbar/navbar.module.ts
@@ -1,12 +1,13 @@
-import { NgModule } from '@angular/core';
+import {NgModule} from '@angular/core';
 import {MatMenuModule} from '@angular/material/menu';
 
-import { CommonModule } from '@angular/common';
+import {CommonModule} from '@angular/common';
 import {TranslateModule} from '@ngx-translate/core';
 import {NavbarComponent} from './navbar.component';
 import {LanguageSelectorComponent} from './language-selector/language-selector.component';
 import {UserMenuComponent} from './user-menu/user-menu.component';
 import {MatButtonModule} from '@angular/material/button';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -24,6 +25,8 @@ import {MatButtonModule} from '@angular/material/button';
     TranslateModule,
     MatMenuModule,
     MatButtonModule,
+    RouterModule
   ]
 })
-export class NavbarModule { }
+export class NavbarModule {
+}
