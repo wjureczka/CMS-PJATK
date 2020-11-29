192aeda5222cd686529201063814d8ca916c5fed
wjureczka <wjureczka@gmail.com>
2020-11-30 00:23:36 +0100

    Init

--- packages/backend/src/users/entities/user.entity.ts
+++
@@ -1,9 +0,0 @@
-import { IsEmail, IsNotEmpty } from 'class-validator';
-
-export class User {
-  @IsEmail()
-  email: string;
-
-  @IsNotEmpty()
-  password: string;
-}
--- packages/backend/src/users/users.controller.ts
+++ packages/backend/src/users/users.controller.ts
@@ -6,18 +6,35 @@ import {
   Put,
   Param,
   Delete,
+  Res,
 } from '@nestjs/common';
+import { StatusCodes } from 'http-status-codes';
+import * as argon from 'argon2';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { Public } from '../auth/public.decorator';
+import { DUPLICATE_KEY } from '../config/mongodb-errors';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @Public()
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto);
+  async create(@Res() response, @Body() createUserDto: CreateUserDto) {
+    const hashedPassword = await argon.hash(createUserDto.password);
+
+    return await this.usersService
+      .create({ ...createUserDto, password: hashedPassword })
+      .then((user) => response.send({ userId: user._id, email: user.email }))
+      .catch((error) => {
+        if (error.code === DUPLICATE_KEY) {
+          return response.code(StatusCodes.CONFLICT).send();
+        }
+
+        return error;
+      });
   }
 
   @Get()
@@ -27,12 +44,12 @@ export class UsersController {
 
   @Get(':id')
   findOne(@Param('email') email: string) {
-    return this.usersService.findOne(email);
+    return this.usersService.findOneByEmail(email);
   }
 
   @Put(':id')
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.update(+id, updateUserDto);
+    return this.usersService.update(id, updateUserDto);
   }
 
   @Delete(':id')
