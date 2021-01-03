23433a973419c54438c19c4c509dd138a6b0b99e
wjureczka <wjureczka@gmail.com>
2021-01-03 14:59:42 +0100

    CS

--- packages/backend-springboot/src/main/java/com/ddebinski/app/auth/payload/request/SignupRequest.java
+++ packages/backend-springboot/src/main/java/com/ddebinski/app/auth/payload/request/SignupRequest.java
@@ -13,6 +13,10 @@ import java.util.Set;
 public class SignupRequest {
 
     @NotBlank
+    @Size(min = 3, max = 20)
+    private String username;
+
+    @NotBlank
     @Size(max = 50)
     @Email
     private String email;
