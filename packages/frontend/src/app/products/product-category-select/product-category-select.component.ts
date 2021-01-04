e2d7c33bdf6261baa529d1f9a56b1d3d668542f5
bdomanski <s16014@pjwstk.edu.pl>
2021-01-04 17:56:14 +0100

    Init product card and category select.

---
+++ packages/frontend/src/app/products/product-category-select/product-category-select.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-product-category-select',
+  templateUrl: './product-category-select.component.html',
+  styleUrls: ['./product-category-select.component.scss']
+})
+export class ProductCategorySelectComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
--- packages/frontend/src/app/products/products.module.ts
+++ packages/frontend/src/app/products/products.module.ts
@@ -1,20 +1,26 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
-
 import {ProductsComponent} from './products.component';
-import {ProductsService} from './products.service';
+import {ProductCardComponent} from './product-card/product-card.component';
+import {MatCardModule} from '@angular/material/card';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatButtonModule} from '@angular/material/button';
+import {NgModule} from '@angular/core';
+import {ProductCategorySelectComponent} from './product-category-select/product-category-select.component';
+import {CommonModule} from '@angular/common';
+
 
 @NgModule({
-  declarations: [ProductsComponent],
+  declarations: [
+    ProductsComponent,
+    ProductCardComponent,
+    ProductCategorySelectComponent,
+  ],
   imports: [
     CommonModule,
-    MatProgressSpinnerModule
-  ],
-  providers: [
-    ProductsService
-  ],
-  exports: []
+    MatCheckboxModule,
+    MatButtonModule,
+    MatCardModule
+  ]
 })
+
 export class ProductsModule {
 }
