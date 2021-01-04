3d60df9408274c258ee3b5ce140f2fbb78e8f6d4
bdomanski <s16014@pjwstk.edu.pl>
2021-01-04 18:12:22 +0100

    Data attach to product card.

--- packages/frontend/src/app/products/product-card/product-card.component.ts
+++ packages/frontend/src/app/products/product-card/product-card.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-product-card',
   templateUrl: './product-card.component.html',
   styleUrls: ['./product-card.component.scss']
 })
-export class ProductCardComponent implements OnInit {
+export class ProductCardComponent {
 
-  constructor() { }
+  @Input() category: string;
 
-  ngOnInit(): void {
-  }
+  @Input() description: string;
+
+  @Input() price: number;
 
 }
