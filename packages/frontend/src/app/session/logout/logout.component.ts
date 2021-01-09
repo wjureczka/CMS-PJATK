d9cfa4c0b56cd2f07e758ffae525af917bb4ea57
wjureczka <wjureczka@gmail.com>
2021-01-09 17:21:01 +0100

    Feature: Add snackbar

--- packages/frontend/src/app/session/logout/logout.component.ts
+++ packages/frontend/src/app/session/logout/logout.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
+import {MatSnackBar} from '@angular/material/snack-bar';
+
 import {AuthService} from '../../core/auth.service';
 
 @Component({
@@ -10,10 +12,13 @@ import {AuthService} from '../../core/auth.service';
 export class LogoutComponent implements OnInit {
 
   constructor(private authService: AuthService,
-              private router: Router) { }
+              private router: Router,
+              private snackbar: MatSnackBar) {
+  }
 
   ngOnInit(): void {
     this.authService.logout();
     this.router.navigate(['/login']);
+    this.snackbar.open('Wylogowano', '', {duration: 3000});
   }
 }
